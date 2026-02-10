db976a32edca0576da840912146b5ed4dc57ef44
Pankaj2025aa05300 <2025aa05300@wilp.bits-pilani.ac.in>
2026-02-10 11:33:03 +0530

    Create App.py

---
+++ App.py
@@ -0,0 +1,9 @@
+!pip install streamlit
+import streamlit as st
+
+st.set_page_config(page_title="ML Assignment 2026", layout="centered")
+
+st.title("ML Assignment 2026")
+st.write("Multiple Classification Models Demo")
+
+st.success("Streamlit app is running successfully!")
